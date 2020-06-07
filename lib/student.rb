7aafcdbab1e7196acbf0d000dd1e80d0f26ed010
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-07 22:57:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,7 +90,9 @@ class Student
         WHERE grade = 10
         LIMIT = ?;
       SQL
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+    end
   end
 
 
