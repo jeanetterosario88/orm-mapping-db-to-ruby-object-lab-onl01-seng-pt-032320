0c0aaef78b21de2e7008148243ca3652c291a73d
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-07 22:48:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
     def self.all
       # retrieve all the rows from the "Students" database
       # remember each row should be a new instance of the Student class
-          sql = <<-SQL
+          sql <<-SQL
             SELECT *
             FROM students
             WHERE name = 9
